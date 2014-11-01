dc8a445854e68bbc4861d177728175ced285f967
Levi Thornton <levithorntonxc@gmail.com>
2014-11-01 13:20:14 -0400

    Removed CV and setup TableViewController

--- TableViewController.m
+++ TableViewController.m
@@ -10,11 +10,9 @@
 #import "CoreDataStack.h"
 #import "Crum.h"
 
-@interface TableViewController ()
+@interface TableViewController () <NSFetchedResultsControllerDelegate>
 
-//<NSFetchedResultsControllerDelegate>
-//
-//@property (nonatomic, strong) NSFetchedResultsController *fetchResultsController;
+@property (nonatomic, strong) NSFetchedResultsController *fetchResultsController;
 
 @end
 
@@ -33,71 +31,71 @@
 
 #pragma mark - tableview controller 
 
-//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-//    // Return the number of sections.
-//    return self.fetchResultsController.sections.count;
-//}
-//
-//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-//    // Return the number of rows in the section.
-//    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchResultsController sections][section];
-//    return [sectionInfo numberOfObjects];
-//}
-//
-//- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
-//    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchResultsController sections][section];
-//    return [sectionInfo name];
-//}
-//
-//- (NSFetchRequest *)entryListFetchRequest {
-//    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Crum"];
-//    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"crumText" ascending:NO]];
-//    
-//    return fetchRequest;
-//}
-//
-//- (NSFetchedResultsController *) fetchResultsController {
-//    if (_fetchResultsController!=nil) {
-//        return _fetchResultsController;
-//    }
-//    
-//    CoreDataStack *coreDataStack = [CoreDataStack defaultStack];
-//    NSFetchRequest *fetchRequest = [self entryListFetchRequest];
-//    
-//    _fetchResultsController = [[NSFetchedResultsController alloc]
-//                               initWithFetchRequest:fetchRequest managedObjectContext:coreDataStack.managedObjectContext sectionNameKeyPath:@"crumText" cacheName:nil];
-//    _fetchResultsController.delegate = self;
-//    
-//    return _fetchResultsController;
-//}
-//
-//- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
-//    [self.tableView endUpdates];
-//}
-//
-//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-//    
-//    static NSString *CellIdentifier = @"Cell";
-//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-//    
-//    Crum *entry = [self.fetchResultsController objectAtIndexPath:indexPath];
-//    cell.textLabel.text = entry.crumText;
-//    
-//    return cell;
-//}
-//
-//-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
-//{
-//    return UITableViewCellEditingStyleDelete;
-//}
-//
-//-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-//    
-//    Crum *entry = [self.fetchResultsController objectAtIndexPath:indexPath];
-//    
-//    CoreDataStack *coreDataStack = [CoreDataStack defaultStack];
-//    [[coreDataStack managedObjectContext] deleteObject:entry];
-//    [coreDataStack saveContext];
-//}
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    // Return the number of sections.
+    return self.fetchResultsController.sections.count;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    // Return the number of rows in the section.
+    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchResultsController sections][section];
+    return [sectionInfo numberOfObjects];
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForHeaderInSection:(NSInteger)section {
+    id <NSFetchedResultsSectionInfo> sectionInfo = [self.fetchResultsController sections][section];
+    return [sectionInfo name];
+}
+
+- (NSFetchRequest *)entryListFetchRequest {
+    NSFetchRequest *fetchRequest = [NSFetchRequest fetchRequestWithEntityName:@"Crum"];
+    fetchRequest.sortDescriptors = @[[NSSortDescriptor sortDescriptorWithKey:@"crumText" ascending:NO]];
+    
+    return fetchRequest;
+}
+
+- (NSFetchedResultsController *) fetchResultsController {
+    if (_fetchResultsController!=nil) {
+        return _fetchResultsController;
+    }
+    
+    CoreDataStack *coreDataStack = [CoreDataStack defaultStack];
+    NSFetchRequest *fetchRequest = [self entryListFetchRequest];
+    
+    _fetchResultsController = [[NSFetchedResultsController alloc]
+                               initWithFetchRequest:fetchRequest managedObjectContext:coreDataStack.managedObjectContext sectionNameKeyPath:@"crumText" cacheName:nil];
+    _fetchResultsController.delegate = self;
+    
+    return _fetchResultsController;
+}
+
+- (void)controllerDidChangeContent:(NSFetchedResultsController *)controller {
+    [self.tableView endUpdates];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    Crum *entry = [self.fetchResultsController objectAtIndexPath:indexPath];
+    cell.textLabel.text = entry.crumText;
+    
+    return cell;
+}
+
+-(UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return UITableViewCellEditingStyleDelete;
+}
+
+-(void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    Crum *entry = [self.fetchResultsController objectAtIndexPath:indexPath];
+    
+    CoreDataStack *coreDataStack = [CoreDataStack defaultStack];
+    [[coreDataStack managedObjectContext] deleteObject:entry];
+    [coreDataStack saveContext];
+}
 
 @end
